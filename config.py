7556f92fa6322d6f96a16687d02c238c5760319b
MoulikMaity <104905898+MoulikMaity@users.noreply.github.com>
2022-05-06 23:50:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+"""
+Api hash c8e7cb62c10c52bfae94ed0e3223103d
+Api id 10585308
+Token 5333295694:AAEpj8fqp6jYpbFRrSyqpUbFgd1vHcX9TOo
+"""
+
+
+API_HASH = os.environ.get("API_HASH")
+API_ID = int(os.environ.get("API_ID"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
